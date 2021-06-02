e7eafc143455fab0abfb2f39f86cfde3e90f07ab
Montather Handal <ali.louy2005@gmail.com>
2021-06-01 22:00:58 -0500

    Fixed?

--- bulkV2/Data/Migrations/20210529203235_man.Designer.cs
+++ bulkV2/Data/Migrations/20210529203235_man.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using bulkV2.Data;
 
 namespace bulkV2.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210529203235_man")]
+    partial class man
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
